550336a94791f7960ec368b914cd98f504c9fd4a
Hepi Soni <hepisonii@gmail.com>
2026-05-24 02:33:28 +0530

    Update interview.js

--- routes/interview.js
+++ routes/interview.js
@@ -27,8 +27,9 @@ interviewRouter.get("/fetch",async (req,res) => {
                             ]);
     console.log("AttemptQuestions: ", attemptQuestions);
     attempt.questions = attemptQuestions.map(q => q._id);
-    await attempt.save();
-    return res.json(attempt);
+    const savedAttempt = await attempt.save();
+    await savedAttempt.populate("questions", "question");
+    return res.json(savedAttempt);
 });
 
 module.exports = interviewRouter;

d8f24bfbb78f4c8f798223eca046a1056ae870b5
Hepi Soni <hepisonii@gmail.com>
2026-05-28 01:11:06 +0530

    allTopScorers

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -2,13 +2,13 @@ const express = require("express");
 const Attempt = require("../models/attempt");
 
 async function handleGetLeaderBoard(req,res){
-   const topScorers = await Attempt.aggregate([
+   const backendTopScorers = await Attempt.aggregate([
   {
     $sort: { totalScore: -1 } // highest first
   },
   {
     $match: {
-      "role": req.user.role,
+      "role": "backend",
     }
   },
   {
@@ -47,7 +47,101 @@ async function handleGetLeaderBoard(req,res){
     $limit: 10
   }
 ]);
-    console.log("Scoreboard: ",topScorers);
+  const frontendTopScorers = await Attempt.aggregate([
+  {
+    $sort: { totalScore: -1 } // highest first
+  },
+  {
+    $match: {
+      "role": "frontend",
+    }
+  },
+  {
+    $group: {
+      _id: "$createdBy",
+      highestScore: { $first: "$totalScore" },
+      attempt_no: { $first: "$attempt_no" },
+      role: {$first: "$role"},
+    }
+  },
+  {
+    $lookup: {
+      from: "users",
+      localField: "_id",
+      foreignField: "_id",
+      as: "user"
+    }
+  },
+  {
+    $unwind: "$user"
+  },
+  {
+    $project: {
+      _id: "$user._id",
+      highestScore: 1,
+      attempt_no: 1,
+      fullname: "$user.fullname",
+      profileImageURL: "$user.profileImageURL",
+      qualifications: "$user.qualifications",
+    }
+  },
+  {
+    $sort: { highestScore: -1 }
+  },
+  {
+    $limit: 10
+  }
+]);
+const cybersecurityTopScorers = await Attempt.aggregate([
+  {
+    $sort: { totalScore: -1 } // highest first
+  },
+  {
+    $match: {
+      "role": "cybersecurity",
+    }
+  },
+  {
+    $group: {
+      _id: "$createdBy",
+      highestScore: { $first: "$totalScore" },
+      attempt_no: { $first: "$attempt_no" },
+      role: {$first: "$role"},
+    }
+  },
+  {
+    $lookup: {
+      from: "users",
+      localField: "_id",
+      foreignField: "_id",
+      as: "user"
+    }
+  },
+  {
+    $unwind: "$user"
+  },
+  {
+    $project: {
+      _id: "$user._id",
+      highestScore: 1,
+      attempt_no: 1,
+      fullname: "$user.fullname",
+      profileImageURL: "$user.profileImageURL",
+      qualifications: "$user.qualifications",
+    }
+  },
+  {
+    $sort: { highestScore: -1 }
+  },
+  {
+    $limit: 10
+  }
+]);
+const topScorers = {
+  frontend: frontendTopScorers,
+  backend: backendTopScorers,
+  cybersecurity: cybersecurityTopScorers,
+}
     return res.json(topScorers);
 }
 

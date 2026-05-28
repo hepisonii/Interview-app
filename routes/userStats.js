133202415d4e3cfdfea6b71ff46279b96d08a796
Hepi Soni <hepisonii@gmail.com>
2026-05-28 17:07:08 +0530

    ..

--- routes/userStats.js
+++ routes/userStats.js
@@ -5,6 +5,6 @@ const {handleGetStats} = require("../controllers/userStats");
 statsRouter.get("/:id/fetch", handleGetStats);
 statsRouter.get("/:id", (req,res) => {
     return res.sendFile(require("path").resolve("./views/userStats.html"))
-})
+});
 
 module.exports = statsRouter;

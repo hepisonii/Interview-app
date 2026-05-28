33bd07932e3b21ab6d624013a3dcaf72d0b6f4b4
Hepi Soni <hepisonii@gmail.com>
2026-05-28 18:21:22 +0530

    Update userStats.js

--- routes/userStats.js
+++ routes/userStats.js
@@ -4,7 +4,7 @@ const {handleGetStats} = require("../controllers/userStats");
 const path = require("path");
 statsRouter.get("/:id/fetch", handleGetStats);
 statsRouter.get("/:id", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/userStats.html"));
+  res.sendFile(path.join(__dirname, "../views/userstats.html"));
 });
 
 module.exports = statsRouter;
